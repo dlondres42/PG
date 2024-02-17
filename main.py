4c04c55b283d8a711c496e6374902a6045aca35c
Tomás N. P. Barros <samotbnp@gmail.com>
2024-02-16 22:16:23 -0300

    pequenas correções, 4a entrega testada e funcionando

--- main.py
+++ main.py
@@ -12,33 +12,58 @@ INF = float(2e9 + 7)
 def main():
     # inputs do usuario
     O = np.array([0, 0, 0])  # origem
-    A = np.array([2, 0, 0])# alvo 
-    up = np.array([0, 1, 0])  # vetor up 
-    dist = 1 # distancia do alvo
+    A = np.array([2, 0, 0])  # alvo
+    up = np.array([0, 1, 0])  # vetor up
+    dist = 1  # distancia do alvo
     hres = vres = 500  # resolucao horizontal e vertical
 
     # calculo dos vetores
     w = normalize(A - O)
-    u = normalize(np.cross(up, w)) 
+    u = normalize(np.cross(up, w))
     v = normalize(np.cross(w, u)) * -1
 
     # objetos
 
-    #test = Triangles(2, 4, np.array([[2, -0.3, 0], [2.5,0.1,0.2], [1.7, 0.5, -0.1], [1.5,0.3,1.2]]), [(0,1,2), (0,2,3)], Color(0, 255, 0))
+    # test = Triangles(2, 4, np.array([[2, -0.3, 0], [2.5,0.1,0.2], [1.7, 0.5, -0.1], [1.5,0.3,1.2]]), [(0,1,2), (0,2,3)], Color(0, 255, 0))
     camera = Camera(O, w, u, v, dist)
 
     objects = [
-        Sphere(np.array([2, 0, 0]), 0.2, Color(0, 255, 0), Material(kd=(0.5,0.5,0.5), ks=(0.25,0.25,0.25), ka=(0.2,0.2,0.2))),
-        #affine_transform(Plane(np.array([1,-0.5,0]), np.array([0,1,0]), Color(0,0,255), Material(ka=(1,1,1))), translation=(0,0,0), rotation_angles=(0,0,0)),
-        affine_transform(Sphere(np.array([2,0,0]), 0.3, Color(0,0,255), Material(kd=(0.5,0.5,0.5), ks=(0.25,0.25,0.25), ka=(0.2,0.2,0.2))), translation=(0,0,0), rotation_angles=(0,0,0)),
-        # Triangles(2, 4, np.array([[4, 1, 0], [4,1,1], [4, 1, -1], [4,0,0]]), [(0,1,3), (1,2,3)], Color(250, 70, 55), Material(ka=(1,1,1)))
+        Sphere(
+            np.array([2, 0, 0]),
+            0.2,
+            Color(0, 255, 0),
+            Material(kd=(0.5, 0.5, 0.5), ks=(0.25, 0.25, 0.25), ka=(0.2, 0.2, 0.2)),
+        ),
+        # affine_transform(Plane(np.array([1,-0.5,0]), np.array([0,1,0]), Color(0,0,255), Material(ka=(1,1,1))), translation=(0,0,0), rotation_angles=(0,0,0)),
+        # affine_transform(
+        #     Sphere(
+        #         np.array([1, 0, 0]),
+        #         0.1,
+        #         Color(0, 0, 255),
+        #         Material(kd=(0.5, 0.5, 0.5), ks=(0.25, 0.25, 0.25), ka=(0.2, 0.2, 0.2)),
+        #     ),
+        #     translation=(0, -0.2, 0),
+        #     rotation_angles=(0, 0, 0),
+        # ),
+        #     affine_transform(
+        #         Triangles(
+        #             2,
+        #             4,
+        #             np.array([[4, 1, 0], [4, 1, 1], [4, 1, -1], [4, 0, 0]]),
+        #             [(0, 1, 3), (1, 2, 3)],
+        #             Color(250, 70, 55),
+        #             Material(kd=(0.5, 0.5, 0.5), ks=(0.25, 0.25, 0.25), ka=(0.2, 0.2, 0.2)),
+        #         ),
+        #         translation=(0, 0, 0),
+        #         rotation_angles=(45, 0, 0),
+        #     ),
     ]
 
     ambient_light = (10, 10, 10)
 
     lights = [
         Light(np.array([0, 5, 5]), np.array([255, 223, 142])),
-        Light(np.array([5, 5, 5]), np.array([255, 255, 255]))
+        Light(np.array([5, 5, 5]), np.array([255, 255, 255])),
     ]
 
     scene = Scene(camera, objects, hres, vres, ambient_light, lights)
@@ -86,7 +111,7 @@ def find_nearest(ray_origin, ray_direction, scene: Scene):
 
 
 def reflect_ray(L, normal):
-    """ 
+    """
     Function used in ray_color in order to compute the
     reflector vector R for the specular component
 
@@ -99,32 +124,32 @@ def reflect_ray(L, normal):
 
     return R
 
+
 def ray_color(ray_origin, ray_direction, scene: Scene):
     t_min, obj_hit = find_nearest(ray_origin, ray_direction, scene)
     color = Color(0, 0, 0)
     if obj_hit is not None:
-        ka = np.array(obj_hit.material.ka) # Ambient coefficient
-        kd = np.array(obj_hit.material.kd) # Diffuse coefficient
-        ks = np.array(obj_hit.material.ks) # Specular coefficient
-        n = np.array(obj_hit.material.eta) # Roughness coefficient
+        ka = np.array(obj_hit.material.ka)  # Ambient coefficient
+        kd = np.array(obj_hit.material.kd)  # Diffuse coefficient
+        ks = np.array(obj_hit.material.ks)  # Specular coefficient
+        n = np.array(obj_hit.material.eta)  # Roughness coefficient
 
         Ia = np.array(scene.ambient_light)
 
         intersection_point = ray_origin + t_min * ray_direction
 
-        
         if isinstance(obj_hit, Sphere):
             normal = normalize(intersection_point - obj_hit.center)
         elif isinstance(obj_hit, Plane):
             normal = obj_hit.normal
         elif isinstance(obj_hit, Triangles):
-            normal = obj_hit.normal_at(intersection_point)  
+            normal = obj_hit.normal_at(intersection_point)
 
         # View vector (direction towards the camera)
         V = normalize(scene.camera.origin - intersection_point)
 
         # Initialize the total color contribution
-        total_color = np.array([0,0,0])
+        total_color = np.array([0, 0, 0])
         total_color = total_color.astype(np.float64)
 
         # Ambient component
@@ -138,12 +163,12 @@ def ray_color(ray_origin, ray_direction, scene: Scene):
             # Diffuse component
             diffuse_dot = max(np.dot(normal, L), 0)
             diffuse_color = kd * light.intensity * diffuse_dot
-            total_color += diffuse_color
+            total_color += diffuse_color % 256
 
             # Specular component
             R = reflect_ray(L, normal)
             specular_dot = max(np.dot(R, V), 0)
-            specular_color = ks * light.intensity * (specular_dot ** n)
+            specular_color = ks * light.intensity * (specular_dot**n)
             total_color += specular_color
 
             color_tuple = (obj_hit.color.r, obj_hit.color.g, obj_hit.color.b)
@@ -154,7 +179,7 @@ def ray_color(ray_origin, ray_direction, scene: Scene):
 
         r, g, b = total_color.clip(0, 255)
         color = Color(r, g, b)
-        #color = obj_hit.color
+        # color = obj_hit.color
     return t_min, color
 
 
@@ -164,67 +189,110 @@ def normalize(vect: np.array):
         return vect
     return vect / norm
 
+
 def affine_transform(obj, translation=(0, 0, 0), rotation_angles=(0, 0, 0)):
     rotation_angles = np.radians(rotation_angles)
 
-    rotation_matrix_x = np.array([
-        [1, 0, 0, 0],
-        [0, np.cos(rotation_angles[0]), -np.sin(rotation_angles[0]), 0],
-        [0, np.sin(rotation_angles[0]), np.cos(rotation_angles[0]), 0],
-        [0, 0, 0, 1]
-    ])
+    rotation_matrix_x = np.array(
+        [
+            [1, 0, 0, 0],
+            [0, np.cos(rotation_angles[0]), -np.sin(rotation_angles[0]), 0],
+            [0, np.sin(rotation_angles[0]), np.cos(rotation_angles[0]), 0],
+            [0, 0, 0, 1],
+        ]
+    )
 
-    rotation_matrix_y = np.array([
-        [np.cos(rotation_angles[1]), 0, np.sin(rotation_angles[1]), 0],
-        [0, 1, 0, 0],
-        [-np.sin(rotation_angles[1]), 0, np.cos(rotation_angles[1]), 0],
-        [0, 0, 0, 1]
-    ])
+    rotation_matrix_y = np.array(
+        [
+            [np.cos(rotation_angles[1]), 0, np.sin(rotation_angles[1]), 0],
+            [0, 1, 0, 0],
+            [-np.sin(rotation_angles[1]), 0, np.cos(rotation_angles[1]), 0],
+            [0, 0, 0, 1],
+        ]
+    )
 
-    rotation_matrix_z = np.array([
-        [np.cos(rotation_angles[2]), -np.sin(rotation_angles[2]), 0, 0],
-        [np.sin(rotation_angles[2]), np.cos(rotation_angles[2]), 0, 0],
-        [0, 0, 1, 0],
-        [0, 0, 0, 1]
-    ])
+    rotation_matrix_z = np.array(
+        [
+            [np.cos(rotation_angles[2]), -np.sin(rotation_angles[2]), 0, 0],
+            [np.sin(rotation_angles[2]), np.cos(rotation_angles[2]), 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1],
+        ]
+    )
 
-    translation_matrix = np.array([
-        [1, 0, 0, translation[0]],
-        [0, 1, 0, translation[1]],
-        [0, 0, 1, translation[2]],
-        [0, 0, 0, 1]
-    ])
+    translation_matrix = np.array(
+        [
+            [1, 0, 0, translation[0]],
+            [0, 1, 0, translation[1]],
+            [0, 0, 1, translation[2]],
+            [0, 0, 0, 1],
+        ]
+    )
 
     # Combine the transformations
-    combined_rotation_matrix = np.dot(rotation_matrix_z, np.dot(rotation_matrix_y, rotation_matrix_x))
+    combined_rotation_matrix = np.dot(
+        rotation_matrix_z, np.dot(rotation_matrix_y, rotation_matrix_x)
+    )
 
     # Apply transformation based on object type
     if isinstance(obj, Sphere):
-        obj_copy = Sphere(obj.center.copy(), obj.radius, obj.color)
+        obj_copy = Sphere(obj.center.copy(), obj.radius, obj.color, obj.material)
 
-        obj_copy.center = np.dot(np.append(obj_copy.center, 1), combined_rotation_matrix.T)[:3]
-        obj_copy.center = np.dot(np.append(obj_copy.center, 1), translation_matrix.T)[:3]
+        obj_copy.center = np.dot(
+            np.append(obj_copy.center, 1), combined_rotation_matrix.T
+        )[:3]
+        obj_copy.center = np.dot(np.append(obj_copy.center, 1), translation_matrix.T)[
+            :3
+        ]
         return obj_copy
     elif isinstance(obj, Plane):
-        obj_copy = Plane(obj.center.copy(), obj.normal.copy(), obj.color)
+        obj_copy = Plane(obj.center.copy(), obj.normal.copy(), obj.color, obj.material)
 
-        obj_copy.center = np.dot(np.append(obj_copy.center, 1), translation_matrix.T)[:3]
-        obj_copy.center = np.dot(np.append(obj_copy.center, 1), combined_rotation_matrix.T)[:3]
-        obj_copy.normal = np.dot(np.append(obj_copy.normal, 0), combined_rotation_matrix.T)[:3]
+        obj_copy.center = np.dot(np.append(obj_copy.center, 1), translation_matrix.T)[
+            :3
+        ]
+        obj_copy.center = np.dot(
+            np.append(obj_copy.center, 1), combined_rotation_matrix.T
+        )[:3]
+        obj_copy.normal = np.dot(
+            np.append(obj_copy.normal, 0), combined_rotation_matrix.T
+        )[:3]
         return obj_copy
     elif isinstance(obj, Triangles):
-        obj_copy = Triangles(obj.num_triangles, obj.num_vertices, obj.vertices.copy(), obj.triangle_index.copy(), obj.color)
+        obj_copy = Triangles(
+            obj.num_triangles,
+            obj.num_vertices,
+            obj.vertices.copy(),
+            obj.triangle_index.copy(),
+            obj.color,
+            obj.material,
+        )
         for i, triangle in enumerate(obj_copy.triangles):
-            triangle.point1 = np.dot(np.append(triangle.point1, 1), translation_matrix.T)[:3]
-            triangle.point1 = np.dot(np.append(triangle.point1, 1), combined_rotation_matrix.T)[:3]
-            triangle.point2 = np.dot(np.append(triangle.point2, 1), translation_matrix.T)[:3]
-            triangle.point2 = np.dot(np.append(triangle.point2, 1), combined_rotation_matrix.T)[:3]
-            triangle.point3 = np.dot(np.append(triangle.point3, 1), translation_matrix.T)[:3]
-            triangle.point3 = np.dot(np.append(triangle.point3, 1), combined_rotation_matrix.T)[:3]
-            triangle.normal = np.cross(triangle.point1 - triangle.point2, triangle.point1 - triangle.point3)
+            triangle.point1 = np.dot(
+                np.append(triangle.point1, 1), translation_matrix.T
+            )[:3]
+            triangle.point1 = np.dot(
+                np.append(triangle.point1, 1), combined_rotation_matrix.T
+            )[:3]
+            triangle.point2 = np.dot(
+                np.append(triangle.point2, 1), translation_matrix.T
+            )[:3]
+            triangle.point2 = np.dot(
+                np.append(triangle.point2, 1), combined_rotation_matrix.T
+            )[:3]
+            triangle.point3 = np.dot(
+                np.append(triangle.point3, 1), translation_matrix.T
+            )[:3]
+            triangle.point3 = np.dot(
+                np.append(triangle.point3, 1), combined_rotation_matrix.T
+            )[:3]
+            triangle.normal = np.cross(
+                triangle.point1 - triangle.point2, triangle.point1 - triangle.point3
+            )
             obj_copy.triangles[i] = triangle
         return obj_copy
     else:
         raise ValueError("Unsupported object type")
+
 
 main()
--- objects.py
+++ objects.py
@@ -3,14 +3,24 @@ from math import sqrt
 
 INF = float(2e9 + 7)
 
+
 class Material:
-    def __init__(self, kd=(0, 0, 0), ks=(0, 0, 0), ka=(0, 0, 0), kr=(0, 0, 0), kt=(0, 0, 0), eta=0):
+    def __init__(
+        self,
+        kd=np.array(0.25, 0.25, 0.25),
+        ks=np.array(0.25, 0.25, 0.25),
+        ka=np.array(0.25, 0.25, 0.25),
+        kr=np.array(0.25, 0.25, 0.25),
+        kt=np.array(0.25, 0.25, 0.25),
+        eta=5,
+    ):
         self.kd = kd  # Diffuse coefficient
         self.ks = ks  # Specular coefficient
         self.ka = ka  # Ambient coefficient
         self.kr = kr  # Reflection coefficient
         self.kt = kt  # Transmission coefficient
         self.eta = eta  # Roughness coefficient
+
 
 class Object:
     def __init__(self, color: tuple, material: Material = Material()):
@@ -31,7 +41,13 @@ class Object:
 
 
 class Sphere(Object):
-    def __init__(self, center: np.array, radius: float, color: tuple, material: Material = Material()):
+    def __init__(
+        self,
+        center: np.array,
+        radius: float,
+        color: tuple,
+        material: Material = Material(),
+    ):
         super().__init__(color, material)
         self.radius = radius
         self.center = center
@@ -48,8 +64,15 @@ class Sphere(Object):
         t2 = (-b - sqrt(determinant)) / (2 * a)
         return min(t1, t2)
 
+
 class Plane(Object):
-    def __init__(self, center: np.array, normal: np.array, color: tuple, material: Material = Material()):
+    def __init__(
+        self,
+        center: np.array,
+        normal: np.array,
+        color: tuple,
+        material: Material = Material(),
+    ):
         super().__init__(color, material)
         self.normal = normal
         self.center = center
@@ -58,17 +81,18 @@ class Plane(Object):
         a = np.dot(self.normal, d_vector)
         if a == 0:
             return INF
-        return (np.dot(self.normal, self.center) - np.dot(self.normal, camera_center))/a
-        
+        return (
+            np.dot(self.normal, self.center) - np.dot(self.normal, camera_center)
+        ) / a
 
-class Triangle():
+
+class Triangle:
     def __init__(self, vertices: np.array) -> None:
-        #self.point1, self.point2, self.point3 = self.ensure_counterclockwise(vertices)
+        # self.point1, self.point2, self.point3 = self.ensure_counterclockwise(vertices)
         self.point1 = vertices[0]
         self.point2 = vertices[1]
         self.point3 = vertices[2]
         self.normal = np.cross(self.point1 - self.point2, self.point1 - self.point3)
-    
 
     @staticmethod
     def ensure_counterclockwise(vertices: np.array) -> np.array:
@@ -76,12 +100,20 @@ class Triangle():
             return vertices[::-1]
         return vertices
 
-
     def __str__(self) -> str:
         return f"ponto1 = {self.point1} \n ponto2 = {self.point2} \n ponto3 = {self.point3} \n normal = {self.normal}"
 
+
 class Triangles(Object):
-    def __init__(self, num_triangles: int, num_vertices: int, vertices: np.array, triangle_index: np.array, color: tuple, material: Material = Material()):
+    def __init__(
+        self,
+        num_triangles: int,
+        num_vertices: int,
+        vertices: np.array,
+        triangle_index: np.array,
+        color: tuple,
+        material: Material = Material(),
+    ):
         super().__init__(color, material)
         self.num_triangles = num_triangles
         self.num_vertices = num_vertices
@@ -93,28 +125,31 @@ class Triangles(Object):
     def define_triangles(self) -> None:
         for i in range(self.num_triangles):
             indexes = self.triangle_index[i]
-            triangle = Triangle([self.vertices[indexes[0]], self.vertices[indexes[1]], self.vertices[indexes[2]]])
+            triangle = Triangle(
+                [
+                    self.vertices[indexes[0]],
+                    self.vertices[indexes[1]],
+                    self.vertices[indexes[2]],
+                ]
+            )
             self.triangles.append(triangle)
-
 
     def intersect(self, camera_center, d_vector) -> float:
         min_t = INF
         for triangle in self.triangles:
-            
+
             v0, v1, v2 = triangle.point1, triangle.point2, triangle.point3
 
-            
             normal = triangle.normal
 
-           
             ndotu = np.dot(normal, d_vector)
             if np.abs(ndotu) < 1e-6:
-                continue  
+                continue
 
             # Compute the intersection point with the plane of the triangle
-            #w = camera_center - v0
-            #(np.dot(self.normal, self.center) - np.dot(self.normal, camera_center))
-            t = (np.dot(normal, v0)  - np.dot(normal, camera_center))/ ndotu
+            # w = camera_center - v0
+            # (np.dot(self.normal, self.center) - np.dot(self.normal, camera_center))
+            t = (np.dot(normal, v0) - np.dot(normal, camera_center)) / ndotu
             intersection_point = camera_center + t * d_vector
 
             # Check if the intersection point is inside the triangle using barycentric coordinates
@@ -122,17 +157,19 @@ class Triangles(Object):
             w1 = v2 - v0
             w2 = intersection_point - v0
 
-            u = (np.dot(w1, w1) * np.dot(w2, w0) - np.dot(w0, w1) * np.dot(w2, w1)) / \
-                (np.dot(w0, w0) * np.dot(w1, w1) - np.dot(w0, w1) ** 2)
+            u = (np.dot(w1, w1) * np.dot(w2, w0) - np.dot(w0, w1) * np.dot(w2, w1)) / (
+                np.dot(w0, w0) * np.dot(w1, w1) - np.dot(w0, w1) ** 2
+            )
 
-            v = (np.dot(w0, w0) * np.dot(w2, w1) - np.dot(w0, w1) * np.dot(w2, w0)) / \
-                (np.dot(w0, w0) * np.dot(w1, w1) - np.dot(w0, w1) ** 2)
+            v = (np.dot(w0, w0) * np.dot(w2, w1) - np.dot(w0, w1) * np.dot(w2, w0)) / (
+                np.dot(w0, w0) * np.dot(w1, w1) - np.dot(w0, w1) ** 2
+            )
 
             if 0 <= u <= 1 and 0 <= v <= 1 and u + v <= 1:
                 min_t = min(min_t, t)
 
         return min_t if min_t != INF else INF
-    
+
     def normal_at(self, intersection_point):
         # Find the triangle closest to the intersection point
         closest_triangle = None
